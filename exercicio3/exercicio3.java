2a616b1ac774a1235de2301c3f3f95d1861877dc
joaquimsatironeto <joaquimsatironeto17@gmail.com>
2026-02-11 19:40:05 -0300

    Terceiro exercício finalizado. Conheci um método novo para calcular a potência dos números.

---
+++ exercicio3/exercicio3.java
@@ -0,0 +1,21 @@
+//3. Dado o tamanho do raio de uma circunferência, calcular a área e o perímetro da mesma.
+
+import java.util.Scanner;
+public class exercicio3 {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner (System.in);
+
+        //Variáveis
+        float raio;
+        float pi = 3.14f;
+        
+        System.out.println("Me diga o raio da sua circunferência e eu direi a área e o perímetro da mesma");
+        raio = sc.nextFloat();
+
+        double area = pi * Math.pow(raio, 2);
+        float perimetro = raio*2;
+
+        System.out.printf("A área da sua circunferência é: %.2f", area);
+        System.out.printf("\nO perímetro da sua circunferêncai é: %.2f", perimetro);
+    }
+}
